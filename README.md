4118051722dd271a547cf32a76ee8355d7c1bc89
Kazuki Matsuda <mats.kazuki@gmail.com>
2023-11-16 00:53:53 +0900

    age

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # parallelcluster-llm
+
 Parallel Cluster for LLM
+
+```sh
+pcluster build-image -c build/dlami-heron.yaml -i dlami-heron
+```
--- scripts/install_heron.sh
+++ scripts/install_heron.sh
@@ -5,4 +5,5 @@ source activate pytorch
 
 # Heron
 wget https://raw.githubusercontent.com/turingmotors/heron/main/requirements.txt
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
+pip install 'transformers>=4.34.1'
